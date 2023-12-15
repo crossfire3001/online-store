92faeef710ff8a9925f6d392f83e7a9d9f9e522b
crossfire3001 <obiwan2035@mail.ru>
2023-12-15 19:28:48 +0500

    Подвал. Структура

--- js/main.js
+++ js/main.js
@@ -28,6 +28,21 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   freeMode: true,
 
+  /* breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },  */
+
   // Navigation arrows
   navigation: {
     nextEl: "#sliderNext",
