26a592afea28a01c033d17a3cb455d74b2fa3d59
crossfire3001 <obiwan2035@mail.ru>
2023-12-18 15:35:52 +0500

    Табы. JS cкрипт переключения

--- js/main.js
+++ js/main.js
@@ -29,19 +29,19 @@ const swiper = new Swiper(".swiper", {
   freeMode: true,
 
   /* breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      },  */
+                                        640: {
+                                          slidesPerView: 2,
+                                          spaceBetween: 20,
+                                        },
+                                        768: {
+                                          slidesPerView: 4,
+                                          spaceBetween: 40,
+                                        },
+                                        1024: {
+                                          slidesPerView: 5,
+                                          spaceBetween: 50,
+                                        },
+                                      },  */
 
   // Navigation arrows
   navigation: {
@@ -49,3 +49,28 @@ const swiper = new Swiper(".swiper", {
     prevEl: "#sliderPrev",
   },
 });
+
+// Tabs
+const tabsBtns = document.querySelectorAll("[data-tab]");
+const tabsProducts = document.querySelectorAll("[data-tab-value]");
+
+for (let btn of tabsBtns) {
+  btn.addEventListener("click", function () {
+    // Убираем активные классы у всех кнопок
+    for (let btn of tabsBtns) {
+      btn.classList.remove("tab-controls__btn--active");
+    }
+
+    // Добавляем активный класс к текущей кнопке
+    this.classList.add("tab-controls__btn--active");
+
+    // Отобразить нужные товары и скрыть ненужные товары
+    for (let product of tabsProducts) {
+      if (product.dataset.tabValue === this.dataset.tab) {
+        product.classList.remove("d-none");
+      } else {
+        product.classList.add("d-none");
+      }
+    }
+  });
+}
